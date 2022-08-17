d392b64510de2f0f5ffb29c7a7325607e34a1850
Jose Luis Quijada <jlquijada@gordiancode.com>
2022-08-17 19:24:19 +0200

    Modify the project to get to use the Person types.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
-import "./style.scss";
+import './style.scss';
+import * as ko from 'knockout';
 
-alert('An alert from index.ts');
+import Person from "./models/person";
+import PersonReadOnly from './components/PersonReadOnly';
+import PersonEditable from './components/PersonEditable';
+
+import filmsBinding from './bindings/filmsBinding';
+
+ko.components.register('person-read-only', PersonReadOnly);
+ko.components.register('person-editable', PersonEditable);
+
+ko.bindingHandlers.films = filmsBinding;
+
+class AppViewModel {
+    person: Person;
+
+    constructor() {
+
+        // These values are hard-coded but could come from a server API request with JSON response
+        this.person = new Person(
+            'Jon',
+            'Keeping',
+            ['The Matrix', 'The Shawshank Redemption', 'Upgrade']
+        )
+    }
+}
 
 
-export { };
+ko.applyBindings(
+    new AppViewModel(),
+    document.getElementById('app'));
